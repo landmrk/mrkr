1b3419832b66addd9ad471301ce4f0bf73f5ec63
Chris Quinn <chrisquinnr@gmail.com>
2020-11-17 11:34:44 +0100

    Updates

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 "use strict";
 
 // Format: https://api.github.com/repos/{owner}/{repo}/issues/
-var GITHUB_ISSUES_LINK = "https://api.github.com/repos/landmrk/mrkr/issues/";
+var GITHUB_ISSUES_LINK = "https://api.github.com/repos/landmrk/mrkr-db/issues/";
 var PATH_SEGMENTS_TO_SKIP = 0;
 var HOST = "mrkr.me";
