b63d943dec987c9877a092feb9d57fcc02a01e42
Chris Quinn <chrisquinnr@gmail.com>
2020-11-17 19:49:18 +0100

    Lmk amends

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // Format: https://api.github.com/repos/{owner}/{repo}/issues/
 const GITHUB_ISSUES_LINK =
-  "https://api.github.com/repos/landmrk/mrkr/issues/";
+  "https://api.github.com/repos/landmrk/mrkr-db/issues/";
 
 const PATH_SEGMENTS_TO_SKIP = 0;
 
