1bf1807f652c7ebdca3d5f9859a3d55b87cc839b
Chris Quinn <chrisquinnr@gmail.com>
2020-11-17 11:25:15 +0100

    mrkr url

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // Format: https://api.github.com/repos/{owner}/{repo}/issues/
 const GITHUB_ISSUES_LINK =
-  "https://api.github.com/repos/nelsontky/gh-pages-url-shortener-db/issues/";
+  "https://api.github.com/repos/landmrk/mrkr/issues/";
 
 const PATH_SEGMENTS_TO_SKIP = 0;
 
-const HOST = "nlsn.cf";
+const HOST = "mrkr.me";
